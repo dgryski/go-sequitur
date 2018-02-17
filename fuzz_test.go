db73ca36e7f3506aba4605ace4afecaf5854dd46
Damian Gryski <damian@gryski.com>
2018-02-17 09:42:13 -0800

    add fuzzing infrastructure

---
+++ fuzz_test.go
@@ -0,0 +1,40 @@
+package sequitur
+
+import (
+	"bytes"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
+
+func TestFuzz(t *testing.T) {
+	corpusFiles, err := filepath.Glob("workdir/corpus/*")
+
+	if err != nil {
+		t.Skipf("error opening test_dir: %v", err)
+		return
+	}
+
+	for _, corpusFile := range corpusFiles {
+		contents, err := ioutil.ReadFile(corpusFile)
+
+		if err != nil {
+			t.Errorf("failed to read %s: %v", corpusFile, err)
+			continue
+		}
+
+		if len(contents) == 0 {
+			continue
+		}
+
+		var b bytes.Buffer
+
+		g, _ := Parse(contents)
+		g.Print(&b)
+		if !bytes.Equal(b.Bytes(), contents) {
+			t.Errorf("mismatch during evaluation for %s", corpusFile)
+		} else {
+			t.Logf("evaluated %s successfully", corpusFile)
+		}
+	}
+}
